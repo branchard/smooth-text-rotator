b1d5bd405d26d7b6681b7f00951c3fc9ed26734b
branchard <benoit.branchard@gmail.com>
2021-10-26 15:15:07 +0200

    test: adding a test of the stop function in the playground

--- playground/index.ts
+++ playground/index.ts
@@ -3,6 +3,10 @@ import {TextRotator} from "../src/main";
 const fonts = ["Forte", "Comic Sans MS", "Arial", "Calibri", "Segoe UI"];
 const sizes = ["16px", "24px", "12px", "18px", "32px"];
 
+function randomNumber(min: number, max: number): number { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const textRotator1Element = document.getElementById('text-rotator-1');
   if (textRotator1Element) {
@@ -10,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
     textRotator.start();
   }
 
-
+  // Test the resize observer
   const textRotator2Element = document.getElementById('text-rotator-2');
   if (textRotator2Element) {
     const textRotator = new TextRotator({element: textRotator2Element, interval: 4000});
@@ -22,6 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 2650);
   }
 
+  // Test the resize observer
   const textRotator3Element = document.getElementById('text-rotator-3');
   if (textRotator3Element) {
     const textRotator = new TextRotator({element: textRotator3Element, interval: 4200});
@@ -33,6 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 2830);
   }
 
+  // Test the resize observer
   const textRotator4Element = document.getElementById('text-rotator-4');
   if (textRotator4Element) {
     const textRotator = new TextRotator({element: textRotator4Element, interval: 4500});
@@ -47,5 +53,28 @@ window.addEventListener('DOMContentLoaded', () => {
         textRotator4Element.parentElement.style.fontSize = fonts[Math.floor(Math.random() * fonts.length)];
       }
     }, 2920);
+  }
+
+  // Test the stop and start
+  const textRotator5Element = document.getElementById('text-rotator-5');
+  if (textRotator5Element) {
+    const textRotator = new TextRotator({element: textRotator5Element, interval: 2520});
+    textRotator.start();
+    console.log("Initial start")
+    let started = true;
+    const startAndStop = () => {
+      if(started){
+        console.log("stop");
+        textRotator.stop();
+        started = false;
+      } else {
+        console.log("start");
+        textRotator.start();
+        started = true
+      }
+      window.setTimeout(startAndStop, randomNumber(3000, 2520 * 5));
+    }
+
+    window.setTimeout(startAndStop, randomNumber(3000, 2520 * 5));
   }
 });
